3caf4438c7d72b8918eb56ede6abf210bbdcc05b
GabrielCv54 <gabriel.cvieira@aluno.faculdadeimpacta.com.br>
2025-03-03 14:50:29 -0300

    Add files via upload

---
+++ Site simples/Projeto/app.js
@@ -0,0 +1,15 @@
+
+    var name = String(window.prompt('Qual seu nome?: '))
+    var club = String(window.prompt('Qual time você torce?: ').toLowerCase())
+    if(club == "São Paulo"){
+        console.log(`${name} Parabéns,você é um cara inteligente!!`)
+    }else if(club == "Palmeiras"){
+         console.log('Porcada sem mundial🐽')
+         alert('Porcada sem mundial🐽')
+    }else if(club == 'Corinthians'){
+        console.log('🐔🐔🐔')
+        alert('🐔🐔')
+    }else if(club == 'Santos'){
+       alert('🧜Sereias da vila')
+       console.log('🧜Sereias da vila')
+    }
